ec21203614fe6fce75cf47a06dc89cf43d1cd3c5
Ville M. Vainio <ville.vainio@basware.com>
2017-05-03 20:03:11 +0300

    first version

--- fsirun/Program.cs
+++ fsirun/Program.cs
@@ -1,15 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace fsirun
+namespace FsiRun
 {
+
     class Program
     {
-        static void Main(string[] args)
+        static ProcessStartInfo GetStartInfo(string cmd, string args) => new ProcessStartInfo
         {
+            CreateNoWindow = false,
+            UseShellExecute = false,
+            FileName = cmd,
+            Arguments = args
+        };
+
+        static int Main(string[] args)
+        {
+            var fsitemplate = @"{0}\Microsoft SDKs\F#\{1}\Framework\v4.0\fsi.exe";
+            var pgmfiles = Environment.GetEnvironmentVariable("programfiles(x86)");
+            var versions = new[] { "4.1", "4.0" };
+            var fsiPath = versions.Select(v => string.Format(fsitemplate, pgmfiles, v))
+                .Where(File.Exists).First();
+
+            var si = GetStartInfo(fsiPath, string.Join(" ", args));
+            var p = Process.Start(si);
+            p.WaitForExit();
+            return p.ExitCode;
         }
     }
 }
